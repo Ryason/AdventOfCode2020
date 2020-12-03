21ca276a58b9d8c08f041b5d0cd26eb9a185c831
Ryan <27cobalt60@gmail.com>
2020-12-03 06:27:47 +0000

    Day 3 added

--- AdventOfCode2020/Day3.cs
+++ AdventOfCode2020/Day3.cs
@@ -10,7 +10,50 @@ namespace AdventOfCode2020
     {
         public static string PartOneOutput(string input)
         {
-            return input;
+            string[] map = input.Split('\n');
+
+            int mapWidth = map[0].Length-1;
+            int mapHeight = map.Length-1;
+            int rowDownCount = 0;
+            bool traveling = true;
+
+            int xPos = 0;
+            int yPos = 0;
+            int treeCount = 0;
+
+
+
+            while (traveling)
+            {
+                if (map[yPos][xPos].ToString() == "#")
+                {
+                    treeCount++;
+                }
+
+                xPos += 1;
+                yPos += 2;
+                rowDownCount += 2;
+                if (xPos>mapWidth)
+                {
+                    xPos = xPos - mapWidth-1;
+                }
+
+                if (yPos > mapHeight)
+                {
+                    yPos = yPos - mapHeight-1;
+                }
+
+                if (rowDownCount>mapHeight)
+                {
+                    traveling = false;
+                }
+                
+
+
+            }
+
+
+            return mapWidth.ToString() + " " + mapHeight.ToString() + "TreeCount = " + treeCount.ToString();
         }
 
         public static string PartTwoOutput(string input)
