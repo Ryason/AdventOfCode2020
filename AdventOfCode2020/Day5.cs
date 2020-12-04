fb65ea5aebfd96d486346ab69550c6f78133b63c
Ryason <46761112+Ryason@users.noreply.github.com>
2020-12-04 18:44:50 +0000

    Added base code in prep for day 5

---
+++ AdventOfCode2020/Day5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    class Day5
+    {
+        #region Part One
+        public static string PartOneOutput(string input)
+        {
+            
+
+            return input;
+        }
+        #endregion
+
+        #region Part Two
+        public static string PartTwoOutput(string input)
+        {
+            
+
+            return input;
+        }
+        #endregion
+
+        #region Functions
+
+        #endregion
+    }
+}
