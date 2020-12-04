83fc9ee4cb2f06b21c4323789108b74ec757ab93
Ryan <27cobalt60@gmail.com>
2020-12-04 11:50:35 +0000

    D4P1 in progress

--- AdventOfCode2020/Day4.cs
+++ AdventOfCode2020/Day4.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,89 @@ namespace AdventOfCode2020
     {
         public static string PartOneOutput(string rawInput)
         {
-            string[] input = rawInput.Split('\n');
+            string[] input = rawInput.Split('\n','\n',':');
 
-            return input[0];
+            ListDictionary passport = new ListDictionary();
+            
+            int count = 0;
+
+
+            string str = "";
+
+            List<string> cleanInput = new List<string>();
+
+            foreach (var item in input)
+            {
+                if (item != "")
+                {
+                    str = str + " " + item;
+                }
+                else
+                {
+                    cleanInput.Add(str);
+                    str = "";
+                }
+
+                if (item == input[input.Length-1])
+                {
+                    cleanInput.Add(str);
+                }
+            }
+
+            foreach (var field in cleanInput)
+            {
+                string[] fields = field.Substring(1).Split(' ');
+                //if (fields.Contains("byr") &&
+                //            fields.Contains("iyr") &&
+                //            fields.Contains("eyr") &&
+                //            fields.Contains("hgt") &&
+                //            fields.Contains("hcl") &&
+                //            fields.Contains("ecl") &&
+                //            fields.Contains("pid") ||
+                //            fields.Contains("byr") &&
+                //            fields.Contains("iyr") &&
+                //            fields.Contains("eyr") &&
+                //            fields.Contains("hgt") &&
+                //            fields.Contains("hcl") &&
+                //            fields.Contains("ecl") &&
+                //            fields.Contains("pid") &&
+                //            fields.Contains("cid"))
+                //{
+                //    count++;
+                //}
+
+                str = "";
+                foreach (var f in fields)
+                {
+                    str = str + " " + f;
+                }
+
+                if (fields.Length < 14)
+                {
+                    //Console.WriteLine("invalid passport detected");
+                    break;
+                }
+                else
+                {
+                    count++;
+                }
+                //else
+                //{
+                    
+                //    if (str.Contains("cid") && str.Contains("pid") || str.Contains("pid"))
+                //    {
+
+                //        Console.WriteLine(str);
+                //        count++;
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("inv : " + str);
+                //    }
+                //}
+            }
+            return count.ToString();
         }
-
         public static string PartTwoOutput(string rawInput)
         {
             string[] input = rawInput.Split('\n');
@@ -23,3 +102,4 @@ namespace AdventOfCode2020
         }
     }
 }
+
