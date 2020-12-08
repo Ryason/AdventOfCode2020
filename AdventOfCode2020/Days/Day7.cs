986859722ec5089755715c67e63050e7a79372ae
Ryan <27cobalt60@gmail.com>
2020-12-08 23:09:51 +0000

    Day 8 Complete (a bit messy, but it works)

--- AdventOfCode2020/Days/Day7.cs
+++ AdventOfCode2020/Days/Day7.cs
@@ -14,44 +14,179 @@ namespace AdventOfCode2020
         {
             string[] cleanIn = input.Split('\n');
 
+            int allowedBagCount = 0;
+
             Dictionary<string, string[]> bagDefinitions = new Dictionary<string, string[]>();
+            
+            Regex rx = new Regex(@"(?<=\d\s)(\w+\s){2}");
 
             foreach (var bagRule in cleanIn)
             {
                 string[] temp = bagRule.Split(new string[] { " bags contain " }, StringSplitOptions.None);
 
                 bagDefinitions.Add(temp[0], temp[1].Split(','));
-            }
 
-            Regex rx = new Regex(@"(?<=\d\s)(\w+\s){2}");
+                for (int i = 0; i < bagDefinitions[temp[0]].Length; i++)
+                {
+
+                    MatchCollection match = rx.Matches(bagDefinitions[temp[0]][i]);
+                    if (match.Count != 0)
+                    {
+                        bagDefinitions[temp[0]][i] = match[0].ToString().Remove(match[0].Length - 1, 1);
+                    }
+                }
+            }
 
             foreach (var item in bagDefinitions)
             {
-                foreach (var val in item.Value)
+                var list = item.Value.ToList();
+                if (CheckContainingBags(list, bagDefinitions))
                 {
-                    MatchCollection match = rx.Matches(val);
-
-                    Console.WriteLine(match[0].ToString().Remove(match[0].Length-1,1));
+                    allowedBagCount++;
                 }
-
-                Console.WriteLine("\n");
+                
             }
 
-            return cleanIn[0];
+            return allowedBagCount.ToString();
         }
         #endregion
 
         #region Part Two
         public static string PartTwoOutput(string input)
         {
-            string[] output = input.Split('\n');
+            string[] cleanIn = input.Split('\n');
+            int bagTotal = 0;
+            
 
-            return output[0];
+            Dictionary<string, string[]> bagDefinitions = new Dictionary<string, string[]>();
+
+            //regex that keeps number of bags (\d\s\w+\s\w+)
+
+            Regex rx = new Regex(@"(\d\s\w+\s\w+)");
+
+            foreach (var bagRule in cleanIn)
+            {
+                string[] temp = bagRule.Split(new string[] { " bags contain " }, StringSplitOptions.None);
+
+                bagDefinitions.Add(temp[0], temp[1].Split(','));
+
+                for (int i = 0; i < bagDefinitions[temp[0]].Length; i++)
+                {
+                    MatchCollection match = rx.Matches(bagDefinitions[temp[0]][i]);
+
+                    if (match.Count != 0)
+                    {
+                        bagDefinitions[temp[0]][i] = match[0].ToString();
+                    }
+                }
+            }
+
+            List<string> bagsToCheck = new List<string>() { "shiny gold" };
+
+            bagTotal += BagsInBag(bagsToCheck, bagDefinitions);
+            
+            
+
+            return bagTotal.ToString();
         }
         #endregion
 
         #region Functions
+        public static int BagsInBag(List<string> bag, Dictionary<string, string[]> bagDefinitions)
+        {
+            List<string> insideBag = new List<string>();
+            foreach (var item in bagDefinitions[bag[0]])
+            {
+                for (int i = 0; i < int.Parse(item.Substring(0, 1)); i++)
+                {
+                    insideBag.Add(item.Substring(2));
+                }
+            }
+            
 
+            int listPosition = 0;
+
+            while (listPosition < insideBag.Count)
+            {
+                
+                foreach (var item in bagDefinitions[insideBag[listPosition]])
+                {
+
+                    if (item == "no other bags.")
+                    {
+                        Console.WriteLine("no other bags.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"adding {int.Parse(item.Substring(0, 1))} {item.Substring(2)}");
+                        for (int i = 0; i < int.Parse(item.Substring(0, 1)); i++)
+                        {
+                            insideBag.Add(item.Substring(2));
+                        }
+                    }
+                    
+                    
+                    
+                }
+                Console.WriteLine("adding 1 to listPosition");
+                listPosition++;
+
+
+            }
+
+            return listPosition;
+        }
+        public static bool CheckContainingBags(List<string> bagCheck, Dictionary<string, string[]> bagDefinitions)
+        {
+            List<string> insideBag = new List<string>();
+            foreach (var item in bagCheck)
+            {
+                insideBag.Add(item);
+            }
+
+            while (!insideBag.Contains("shiny gold"))
+            {
+                foreach (var bag in insideBag.ToList())
+                {
+                    if (bag == "no other bags.")
+                    {
+
+                    }
+                    else
+                    {
+                        foreach (var item in ContentsOfBag(bag,bagDefinitions))
+                        {
+                            if (!insideBag.Contains(item))
+                            {
+                                insideBag.Add(item);
+                            }
+                        }
+
+                        insideBag.Remove(bag);
+                    }
+                }
+
+                if (insideBag.Count == 1 && insideBag[0] == "no other bags.")
+                {
+                    break;
+                }
+            }
+
+            if (insideBag.Contains("shiny gold"))
+            {
+                return true;
+            }
+
+            
+            
+
+            return false;
+        }
+
+        public static List<string> ContentsOfBag(string bag, Dictionary<string, string[]> bagDefinitions)
+        {
+            return bagDefinitions[bag].ToList();
+        }
         #endregion
     }
 }
--- AdventOfCode2020/Days/Day8.cs
+++ AdventOfCode2020/Days/Day8.cs
@@ -11,23 +11,252 @@ namespace AdventOfCode2020
         #region Part One
         public static string PartOneOutput(string input)
         {
-            string[] output = input.Split('\n');
+            string[] operations = input.Split('\n');
 
-            return output[0];
+            int acc = 0;
+            int opPos = 0;
+
+            List<int> opRan = new List<int>();
+
+            bool looping = false;
+
+            while (opPos < operations.Length)
+            {
+                string[] opArray = operations[opPos].Split(' ');
+                
+                string op = opArray[0];
+                string val = opArray[1];
+
+                switch (op)
+                {
+                    case "nop":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            opPos++;
+                        }
+                        break;
+                    case "acc":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            acc += int.Parse(val);
+                            opPos++;
+                        }
+                        break;
+                    case "jmp":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            opPos += int.Parse(val);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                if (looping)
+                {
+                    Console.WriteLine(acc);
+                    break;
+                }
+            }
+
+            return acc.ToString();
         }
         #endregion
 
         #region Part Two
         public static string PartTwoOutput(string input)
         {
-            string[] output = input.Split('\n');
+            string[] operations = input.Split('\n');
 
-            return output[0];
+            for (int i = 0; i < operations.Length; i++)
+            {
+                string[] operationsModified = new string[operations.Length];
+
+                operations.CopyTo(operationsModified,0);
+
+                if (operationsModified[i].Contains("nop"))
+                {
+                    Console.WriteLine($"changing nop to jump at pos {i}");
+                    operationsModified[i] = operationsModified[i].Replace("nop", "jmp");
+                }
+                else
+                {
+                    if (operationsModified[i].Contains("jmp"))
+                    {
+                        Console.WriteLine($"changing jmp to nop at pos {i}");
+                        operationsModified[i] = operationsModified[i].Replace("jmp", "nop");
+                    }
+                }
+
+                if (Loops(operationsModified))
+                {
+                    Console.WriteLine($"changing line {i} returns a LOOP");
+                    operationsModified = operations;
+                }
+                else
+                {
+                    return ReturnAcc(operationsModified).ToString();
+                    
+                }
+            }
+
+            return "";
         }
         #endregion
 
         #region Functions
+        public static bool Loops(string[] input)
+        {
+            string[] operations = input;
 
+            int acc = 0;
+
+            int opPos = 0;
+
+            List<int> opRan = new List<int>();
+            bool looping = false;
+
+            while (opPos < operations.Length)
+            {
+                string[] opArray = operations[opPos].Split(' ');
+
+                string op = opArray[0];
+                string val = opArray[1];
+
+                switch (op)
+                {
+                    case "nop":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            opPos++;
+                        }
+                        break;
+                    case "acc":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            acc += int.Parse(val);
+                            opPos++;
+                        }
+                        break;
+                    case "jmp":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            opPos += int.Parse(val);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                if (looping)
+                {
+                    break;
+                }
+            }
+            return looping;
+        }
+
+        public static int ReturnAcc(string[] input)
+        {
+            string[] operations = input;
+
+            int acc = 0;
+
+            int opPos = 0;
+
+            List<int> opRan = new List<int>();
+            bool looping = false;
+
+            while (opPos < operations.Length)
+            {
+                string[] opArray = operations[opPos].Split(' ');
+
+                string op = opArray[0];
+                string val = opArray[1];
+
+                switch (op)
+                {
+                    case "nop":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            opPos++;
+                        }
+                        break;
+                    case "acc":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            acc += int.Parse(val);
+                            opPos++;
+                        }
+                        break;
+                    case "jmp":
+                        if (opRan.Contains(opPos))
+                        {
+                            looping = true;
+                            break;
+                        }
+                        else
+                        {
+                            opRan.Add(opPos);
+                            opPos += int.Parse(val);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                if (looping)
+                {
+                    break;
+                }
+            }
+            return acc;
+        }
         #endregion
     }
 }
