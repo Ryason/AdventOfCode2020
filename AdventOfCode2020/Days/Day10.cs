cbf07b365907c5c68f2ca4ed6713902195f1c87d
Ryan <27cobalt60@gmail.com>
2020-12-10 15:48:58 +0000

    Completed Day 10 Part 1

--- AdventOfCode2020/Days/Day10.cs
+++ AdventOfCode2020/Days/Day10.cs
@@ -9,11 +9,41 @@ namespace AdventOfCode2020
     class Day10
     {
         #region Part One
-        public static string PartOneOutput(string input)
+        public static string PartOneOutput(string rawInput)
         {
-            string[] output = input.Split('\n');
+            string[] input = rawInput.Split('\n');
 
-            return output[0];
+            List<int> joltages = new List<int>();
+            joltages.Add(0);
+            foreach (string jolt in input)
+            {
+                joltages.Add(int.Parse(jolt));
+            }
+
+            joltages.Sort();
+            joltages.Add(joltages.Max() + 3);
+
+            Dictionary<int, int> differences = new Dictionary<int, int>();
+            
+            differences.Add(1, 0);
+            differences.Add(3, 0);
+
+            for (int i = 1; i < joltages.Count; i++)
+            {
+                switch (joltages[i] - joltages[i-1])
+                {
+                    case 1:
+                        differences[1]++;
+                        break;
+                    case 3:
+                        differences[3]++;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return $"Differences of 1 = {differences[1]}\nDifferences of 3 = {differences[3]}\nMultiplied = {differences[1] * differences[3]}";
         }
         #endregion
 
