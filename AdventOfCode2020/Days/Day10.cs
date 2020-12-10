07dbe28e80a9994c22dcb3d9fced1d8935d745ef
Ryason <46761112+Ryason@users.noreply.github.com>
2020-12-10 21:45:04 +0000

    Day 10 Complete

--- AdventOfCode2020/Days/Day10.cs
+++ AdventOfCode2020/Days/Day10.cs
@@ -48,11 +48,76 @@ namespace AdventOfCode2020
         #endregion
 
         #region Part Two
-        public static string PartTwoOutput(string input)
+        public static string PartTwoOutput(string rawInput)
         {
-            string[] output = input.Split('\n');
+            string[] input = rawInput.Split('\n');
 
-            return output[0];
+            List<int> joltages = new List<int>();
+
+            joltages.Add(0);
+
+            foreach (string jolt in input)
+            {
+                joltages.Add(int.Parse(jolt));
+            }
+
+            joltages.Sort();
+            joltages.Add(joltages.Max() + 3);
+
+            //find what numbers in the sequence are mandatory and what can be altered.
+            //TRIBONACCI SERIES!!!
+
+            List<long> paths = new List<long>();
+            List<long> pathSum = new List<long>();
+
+            //first 0 jolt adapter has one path too it(itself).
+            paths.Add(1);
+            pathSum.Add(1);
+            
+            //determine how many previous adapters one can reach with the given restrictions
+            //(a difference of joltage of no more than 3 jolts))
+            for (int i = 1; i < joltages.Count(); i++)
+            {
+                int pathCount = 0;
+
+                long joltage = joltages[i];
+
+                for (int j = i-1; j > j-3; j--)
+                {
+                    if (j < 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        if (joltage - joltages[j] <= 3)
+                        {
+                            pathCount++;
+                        }
+                    }
+                }
+
+                paths.Add(pathCount);
+            }
+
+            for (int i = 1; i < paths.Count(); i++)
+            {
+                long sum = 0;
+
+                for (int j = i-1; j >= i-paths[i]; j--)
+                {
+                    sum += pathSum[j];
+                }
+
+                pathSum.Add(sum);
+            }
+
+            for (int i = 0; i < joltages.Count(); i++)
+            {
+                Console.WriteLine($"Adapter {i + 1}: {joltages[i]}, Paths: {paths[i]}, PathSum: {pathSum[i]}");
+            }
+
+            return $"Number of combinations: {pathSum[pathSum.Count() - 1]}";
         }
         #endregion
 
