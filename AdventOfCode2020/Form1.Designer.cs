1c7cf6145c487f413f0cec049544a9505d29024a
Ryan <27cobalt60@gmail.com>
2020-12-15 13:36:09 +0000

    Changed day selector to day 16

--- AdventOfCode2020/Form1.Designer.cs
+++ AdventOfCode2020/Form1.Designer.cs
@@ -67,7 +67,7 @@
             this.dayNumber.Size = new System.Drawing.Size(47, 20);
             this.dayNumber.TabIndex = 1;
             this.dayNumber.Value = new decimal(new int[] {
-            14,
+            16,
             0,
             0,
             0});
